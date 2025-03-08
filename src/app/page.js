8df21687427856f21894e24c066de3ae65ad42ab
mdabdulkyum1 <kyummdabdul@gmail.com>
2025-03-08 23:43:54 +0600

    add compenents folder

--- src/app/page.js
+++ src/app/page.js
@@ -23,6 +23,10 @@ export default function Home() {
           <li>Save and see your changes instantly.</li>
         </ol>
 
+        <div>
+           <h1>Abdul Kyum</h1>
+        </div>
+
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
