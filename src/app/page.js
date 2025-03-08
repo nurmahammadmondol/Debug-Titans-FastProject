93832d0353c4a98b65b1a3ec72e3b68a89118c4e
unknown <nurmahannadmondol>
2025-03-09 01:10:45 +0430

    Update navbar part (author robi)

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,12 @@
-import Image from "next/image";
+import Image from 'next/image';
+import Navbar from './Components/Navbar';
 
 export default function Home() {
   return (
     <div>
+      <div>
+        <Navbar></Navbar>
+      </div>
       <h1> hello</h1>
     </div>
   );
