184ea227c3cb2211a285d04b41decf27db591cbe
ashrafulhossain1 <ahmeadashraful@gmail.com>
2025-03-09 23:12:08 +0600

    demo section added more

--- src/app/Components/Demo/Demo.jsx
+++ src/app/Components/Demo/Demo.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 const Demo = () => {
     return (
         <div>
+            <h3 className='text-5xl text-center'>Demo</h3>
+            <h3 className='text-5xl text-center'> Demo</h3>
+            <h3 className='text-5xl text-center'> Demo</h3>
             <h3 className='text-5xl text-center'> Demo</h3>
         </div>
     );
