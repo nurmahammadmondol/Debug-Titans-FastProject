699bcc095e4a69620d4dfa69a24a756b0fcb55b4
ashrafulhossain1 <ahmeadashraful@gmail.com>
2025-03-09 23:04:59 +0600

    demo section added

---
+++ src/app/Components/Demo/Demo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Demo = () => {
+    return (
+        <div>
+            <h3 className='text-5xl text-center'> Demo</h3>
+        </div>
+    );
+};
+
+export default Demo;
