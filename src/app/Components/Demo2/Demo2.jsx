dd2a18491e71c285ba6db1f8f4335a228e2a46d1
ashrafulhossain1 <ahmeadashraful@gmail.com>
2025-03-09 23:25:21 +0600

    demo2 task complete

---
+++ src/app/Components/Demo2/Demo2.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Demo2 = () => {
+    return (
+        <div>
+            <h2 className='bg-green-500 text-6xl py-4'>Demo-2</h2>
+        </div>
+    );
+};
+
+export default Demo2;
